63a58aa03a5e9bd7e41ba100b337d31bb56c0174
AlejoIzzo <izzoalejo@gmail.com>
2026-09-17 23:54:42 -0300

    mostrar fecha y hora exacta de pujas en sala de subastas

--- frontend/src/components/pujaCard.ts
+++ frontend/src/components/pujaCard.ts
@@ -1,12 +1,12 @@
 import { formatNumber } from "../helpers/stringHelpers"
 import type { PujaDTO } from "../models/pujaTypes"
 import type { UsuarioDTO } from "../models/usuarioTypes"
-import htmlTemplate from "./puja-card.html?raw" 
+import htmlTemplate from "./puja-card.html?raw"
 const template = new DOMParser().parseFromString(htmlTemplate, "text/html").getElementById("template") as HTMLTemplateElement
 
 export function createPujaCardElement(dto: PujaDTO, usuario: UsuarioDTO) {
     const clone = template.content.cloneNode(true) as DocumentFragment
-    
+
     // const container = clone.querySelector('.puja-card')! as HTMLElement
     const pujaContainer = clone.querySelector('.puja-card')! as HTMLElement
     const pujaComprador = clone.querySelector('.puja-comprador-nombre')!
@@ -16,7 +16,7 @@ export function createPujaCardElement(dto: PujaDTO, usuario: UsuarioDTO) {
     pujaComprador.textContent = dto.compradorNombre
     pujaFecha.textContent = `${getPujaDate(new Date(dto.fecha))}`
     pujaMonto.textContent = `$ ${formatNumber(dto.monto)}`
-    
+
     pujaFecha.dataset.fecha = dto.fecha
     pujaContainer.dataset.compradorId = String(dto.compradorId)
     if (dto.compradorId === usuario.id)
@@ -31,7 +31,7 @@ export function refreshUserPujas(usuario: UsuarioDTO) {
     pujaContainers.forEach((x: any) => {
         if (Number(x.dataset.compradorId) === usuario.id)
             x.classList.add("of-user")
-        else 
+        else
             x.classList.remove("of-user")
     })
 }
@@ -44,21 +44,6 @@ export function refreshPujasDates() {
     })
 }
 
-function getPujaDate(fecha: Date) : string {
-    const diff = Date.now() - fecha.getTime();
-    const totalSegundos = Math.floor(diff / 1000);
-
-    const dias = Math.floor(totalSegundos / 86400);
-    if (dias > 0)
-        return `Hace ${dias} ${dias > 1 ? "días" : "día"}`;
-
-    const horas = Math.floor(totalSegundos / 3600);
-    if (horas > 0)
-        return `Hace ${horas} ${horas > 1 ? "horas" : "hora"}`;
-
-    const minutos = Math.floor(totalSegundos / 60);
-    if (minutos > 0)
-        return `Hace ${minutos} ${minutos > 1 ? "minutos" : "minuto"}`;
-    
-    return `Hace unos segundos`
+function getPujaDate(fecha: Date): string {
+    return fecha.toLocaleString();
 }
